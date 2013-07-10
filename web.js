e20725572ed90e68357772958451ded05c635ae2
Mikkel Garcia <mikkel@255bits.com>
2013-07-09 23:21:16 -0600

    fs.readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  var file = fs.readFileSync("index.html", "utf-8");
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
